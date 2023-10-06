fcf6fad04261ffee6ac6bffbae9d47bcb0f09ae8
Sebastian Montero <sebastian.montero@gmail.com>
2023-10-06 08:46:38 -0600

    Updated the bdk version to the latest.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
+use bdk::bitcoin::Txid;
 
-
-use bdk::bitcoin::{Txid};
-use bdk::chain::keychain::WalletUpdate;
-
-use bdk_esplora::{esplora_client,EsploraAsyncExt};
+use bdk::wallet::Update;
+use bdk_esplora::{esplora_client, EsploraAsyncExt};
 use std::str::FromStr;
-
-
 
 #[tokio::main]
 async fn main() {
@@ -15,41 +11,52 @@ async fn main() {
     let endpoint = "https://mempool.space/testnet/api/";
     let request_stop_gap = 40;
     let parallel_requests = 5;
-    let mut wallet = bdk::Wallet::new_no_persist(
-        descriptor,
-        None,
-        bdk::bitcoin::Network::Testnet,
-    ).unwrap();
-    let blockchain = esplora_client::Builder::new(endpoint).build_async().unwrap();
+    let mut wallet =
+        bdk::Wallet::new_no_persist(descriptor, None, bdk::bitcoin::Network::Testnet).unwrap();
+    let blockchain = esplora_client::Builder::new(endpoint)
+        .build_async()
+        .unwrap();
 
     let prev_tip = wallet.latest_checkpoint();
-    let keychain_spks = wallet
-        .spks_of_all_keychains();
+    let keychain_spks = wallet.spks_of_all_keychains();
     let (update_graph, last_active_indices) = blockchain
-    .update_tx_graph(keychain_spks, None, None, request_stop_gap, parallel_requests)
-    .await.unwrap();
+        .scan_txs_with_keychains(
+            keychain_spks,
+            None,
+            None,
+            request_stop_gap,
+            parallel_requests,
+        )
+        .await
+        .unwrap();
     let missing_heights = wallet.tx_graph().missing_heights(wallet.local_chain());
-    let chain_update = blockchain.update_local_chain(prev_tip, missing_heights).await.unwrap();
-    let update = WalletUpdate {
+    let chain_update = blockchain
+        .update_local_chain(prev_tip, missing_heights)
+        .await
+        .unwrap();
+    let update = Update {
         last_active_indices,
         graph: update_graph,
-        ..WalletUpdate::new(chain_update)
+        chain: Some(chain_update),
     };
     wallet.apply_update(update).unwrap();
     wallet.commit().unwrap();
-    
-    let txid = Txid::from_str("c986542760cce19005b436fc45675a43819084bf37f683dae06e4816e77e8e9f").unwrap();
+
+    let txid =
+        Txid::from_str("c986542760cce19005b436fc45675a43819084bf37f683dae06e4816e77e8e9f").unwrap();
     let canonical_tx = wallet.get_tx(txid).unwrap();
     dbg!(canonical_tx.clone());
     dbg!(canonical_tx.chain_position);
     dbg!(canonical_tx.chain_position.is_confirmed());
-    let confirmation_time: bdk::chain::ConfirmationTime = canonical_tx.chain_position.cloned().into();
+    let confirmation_time: bdk::chain::ConfirmationTime =
+        canonical_tx.chain_position.cloned().into();
     match confirmation_time {
         bdk::chain::ConfirmationTime::Confirmed { height, time } => {
             dbg!(height);
             dbg!(time);
-            
         }
-        bdk::chain::ConfirmationTime::Unconfirmed { last_seen } => { dbg!(last_seen); }
+        bdk::chain::ConfirmationTime::Unconfirmed { last_seen } => {
+            dbg!(last_seen);
+        }
     }
 }
